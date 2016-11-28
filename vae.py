b26a1169b91965eef3804b4546c2bde8ef529061
TrofimovAssya <assya.trofimov@umontreal.ca>
2016-11-27 22:20:05 -0500

    cleanup code and make warp function

--- vae.py
+++ vae.py
@@ -9,7 +9,6 @@ from keras import objectives
 My implementation of a Variational Autoencoder
 Inspired by Francois Chollet (Keras) https://github.com/fchollet/keras
 and Carl Doersch (Tutorial on VAE) https://arxiv.org/pdf/1606.05908v2.pdf
-
 Data used: 100 transcriptomes (RNA-Seq data) from Lung and Kidney cells obtained from The Cancer Genome Atlas (https://tcga-data.nci.nih.gov)
 Input: 20532 gene expression values per sample.
 Latent space: 2D, Gaussian.
@@ -27,12 +26,19 @@ nb_epoch = 10
 epsilon_std = 1.0
 
 
+"""
+Plotting function - totally optionnal!
+
+"""
+def draw_graph(data, classes):
+    plt.scatter(data[:, 0], data[:, 1], c=classes)
+    plt.colorbar()
+    plt.show()
 
 """
 Loading data.
 Y is a file containing the transcriptomes of Lung cells
 Z is a file containing the transcriptomes of Kidney cells.
-
 """
 
 data1 = numpy.loadtxt(".gitignore/y")
@@ -47,13 +53,19 @@ x_train = x_train.astype('float32') / numpy.max(x_train)
 x_train = x_train.reshape((len(x_train), numpy.prod(x_train.shape[1:])))
 
 
+original_dim = 20532
+batch_size = 10
+latent_dim = 2
+hidden_dim = 1500
+nb_epoch = 10
+epsilon_std = 2.0
+
 
 """
 Key players in the VAE:
 X are inputs. They are the data that goes into the model.
 z are values in latent space. 
 For each X there is a z in latent space. The neural network learns to chose a good z to describe each X (reconstruct)
-
 We want to maximise the sampled z for X, to make sure the z chosen by the model accurately describes X. This means we want to maximise P(X|z).
 We can solve this by sampling but it might take a lot of samples, since Z domain is vast and so P(X|z) ~ 0 most of the time.
 Since p(x) \approx   \frac{1}{n}\sum_{i}p(X|z_{i}) is too vast, we could define another distribution Q(z|X), that is contained in P(X|z) and is a subset.
@@ -66,15 +78,14 @@ We will then make Q(z|X) most likely with P(X|z), by minimizing the Kullback-Lie
 Creating a sampling function, that samples for z  through Q (which we chose to be Gaussian (mean=0, covar=Identity))
 """
 def sampling(args):
-    z_mean, z_log_var = args
-    epsilon = K.random_normal(shape=(batch_size, latent_dim), mean=0.,
-                              std=epsilon_std)
-    return z_mean + K.exp(z_log_var / 2) * epsilon
+z_mean, z_log_var = args
+epsilon = K.random_normal(shape=(batch_size, latent_dim), mean=0.,
+                          std=epsilon_std)
+return z_mean + K.exp(z_log_var / 2) * epsilon
 
 """
 Defining layers (input, hidden, z_mean and z_log_var (parameters of the latent space))
 """
-
 x = Input(batch_shape=(batch_size, original_dim))
 h = Dense(hidden_dim, activation='relu')(x)
 z_mean = Dense(latent_dim)(h)
@@ -90,6 +101,19 @@ h_decoded = decoder_h(z)
 x_decoded_mean = decoder_mean(h_decoded)
 
 """
+Building a model to decode from the latent space
+"""
+decoder_input = Input(shape=(latent_dim,))
+_h_decoded = decoder_h(decoder_input)
+_x_decoded_mean = decoder_mean(_h_decoded)
+decoder_from_z = Model(decoder_input, _x_decoded_mean)
+
+"""
+Making an encoder that will transfer inputs to latent space.
+"""
+encoder = Model(x, z_mean)
+
+"""
 Defining the loss. We want to penalize two things:
 1 - The reconstruction loss from X to reconstructed X. (Autoencoder part of the VAE)
 2 - The Kullback-Liebler Divergence between Q(z|X) and P(z) (making sure z is accurately chosen to represent X)
@@ -100,28 +124,15 @@ def vae_loss(x, x_decoded_mean):
     return xent_loss + kl_loss
 
 """
-Instantiation of the model.
+Instantiation of the model and fitting the data
 """
 vae = Model(x, x_decoded_mean)
 vae.compile(optimizer='rmsprop', loss=vae_loss)
-
-
-vae.fit(x_train, x_train,
-        shuffle=True,
-        nb_epoch=nb_epoch,
-        batch_size=batch_size)
-
-
-"""
-Making an encoder that will transfer inputs to latent space.
-"""
-encoder = Model(x, z_mean)
+vae.fit(x_train, x_train, shuffle=True, nb_epoch=nb_epoch, batch_size=batch_size)
 
 """
 Encoding the input data for it's representation in 2D latent space + plotting the data.
 """
-x_train_encoded = encoder.predict(x_train)
-plt.scatter(x_train_encoded[:, 0], x_train_encoded[:, 1], c=y_train)
-plt.colorbar()
-plt.show()
 
+x_train_encoded = encoder.predict(x_train)
+#draw_graph(x_train_encoded,y_train)
---
+++ warp.py
@@ -0,0 +1,88 @@
+from keras.layers.core import Dense, Dropout, Activation, Merge
+from keras.optimizers import SGD, Adam, RMSprop, Adagrad
+from keras.utils import np_utils
+from keras.models import Sequential , model_from_json, Model
+from keras.callbacks import Callback
+from keras.regularizers import l2, activity_l2, l1
+from keras.initializations import glorot_normal, identity
+from keras.layers import Input, merge
+from keras import objectives
+
+from numpy import random
+import numpy
+from sklearn import preprocessing
+numpy.random.seed(32689)
+
+
+def warp(data_x,y_train):
+	batch_size = 10
+	nb_epoch = 10
+	x_train = data_x
+
+	###construct the model
+	model = Sequential()
+	model.add(Dense(2, input_shape=(2,), activation='tanh'))
+	model.add(Dense(2,activation='softmax'))
+
+	###compile and summarize model
+	model.summary()
+	model.compile(loss='binary_crossentropy',
+	          optimizer=RMSprop(lr=1e-2),
+	          metrics=['accuracy'])
+	###training
+	model.fit(x_train, y_train, batch_size=batch_size, nb_epoch=nb_epoch, verbose=1, shuffle=True)
+
+	##loading warping datasets
+	y_train2 = numpy.copy(y_train)
+	for i in xrange(len(y_train2)):
+		if y_train2[i][1] == 0:
+			y_train2[i] = [0.,1.]
+
+
+
+	###construction of the warp setup with added bias layer in the beggining
+	warp = Sequential()
+	warp.add(Dense(2,input_shape = (2,), init='identity'))
+	warp.add(Dense(2, input_shape = (2,),  activation='tanh', weights = [model.layers[0].W.get_value(), model.layers[0].b.get_value()]))
+	warp.add(Dense(2, activation = 'softmax', weights = [model.layers[1].W.get_value(), model.layers[1].b.get_value()]))
+
+	for i in xrange(len(model.layers)):
+		warp.layers[i+1].trainable = False
+
+
+	### making sure only bias is allowed to change
+	warp.layers[0].non_trainable_weights.append(warp.layers[0].trainable_weights[0])
+	warp.layers[0].trainable_weights = warp.layers[0].trainable_weights[1:]
+
+
+	warp.summary()
+	warp.compile(loss='binary_crossentropy', optimizer=RMSprop(lr=1e-3), metrics=['accuracy'])
+
+	def stop_training(model, x_train, y_train2, bias):
+		return numpy.argmax(y_train2) == numpy.argmax(model.predict(x_train+bias))
+
+	nb_epoch=300
+	sgd = RMSprop(lr=1e-3)
+	biases = []
+	"""  make a Callback for monitoring for each point"""
+	for point in xrange(x_train.shape[0]):
+		print "data point #" + str(point)
+		print " "
+		### setting the biases to zero between each data point
+		warp.layers[0].b.set_value(numpy.zeros(warp.layers[0].b.get_value().shape[0],dtype="float32"))
+		### warping....
+		epochs=0
+		while epochs<nb_epoch:
+			if numpy.argmax(y_train2[point:point+1][0]) == numpy.argmax(model.predict(x_train[point:point+1]+warp.layers[0].b.get_value())):
+				break
+			print "Epoch "+str(epochs) + " - patient " +str(point)
+			print " "
+			print(model.predict(x_train[point:point+1]))
+			print((warp.predict(x_train[point:point+1])))
+			warp.fit(x_train, [y_train2],
+	                batch_size=batch_size, nb_epoch=1,
+	                verbose=0, shuffle=True)
+			epochs+=1
+		biases.append(warp.layers[0].b.get_value())
+	return biases
+
